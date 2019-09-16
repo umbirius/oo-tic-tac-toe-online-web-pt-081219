46d7a73a1da00e4d6a912bb0e11fb6ec79db9794
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-16 14:50:09 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -239,8 +239,6 @@ class TicTacToe
         return "X"
       elsif @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
         return "O"
-      else 
-        nil
       end 
     end 
     
