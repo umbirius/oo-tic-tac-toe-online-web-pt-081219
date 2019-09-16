964a368cb97c46932e9065c202a949295375ec26
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-16 14:49:25 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -234,7 +234,7 @@ class TicTacToe
   
   def winner? 
     win_combo = nil
-    WIN_COMBINATIONS.each do |combo|
+    WIN_COMBINATIONS.detect do |combo|
       if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X"
         return "X"
       elsif @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
